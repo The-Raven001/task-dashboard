e4dfdff916e1d02969a487673a2d3b35de599beb
Gabriel <gacm95218@gmail.com>
2026-05-04 23:52:15 -0400

    Fix to App.tsx to attempt to fix routing for vercel deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,6 @@ import { NotFound } from "./pages/NotFound";
 import DashboardLayout from "./layouts/DashboardLayout";
 import PublicLayout from "./layouts/PublicLayout";
 import { Toaster } from "react-hot-toast";
-import { Navigate } from "react-router-dom";
 
 function App() {
   return (
