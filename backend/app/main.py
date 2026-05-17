4a448822acb7a9bce5af802b3636ea189f1ddaad
Gabriel <gacm95218@gmail.com>
2026-05-17 14:21:02 -0400

    update backend requirements

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "https://task-dashboard-gamma-self.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
